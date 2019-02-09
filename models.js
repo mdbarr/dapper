3eb1ae77d836d9f0b663c8848495b8e49a4d0710
Mark Barr <mark@hyperingenuity.com>
2019-02-09 00:51:08 -0500

    Stub functions for planned data models.

--- models.js
+++ models.js
@@ -3,8 +3,12 @@
 function Models() {
   const self = this;
 
+  self.node = function() {};
+  self.tree = function() {};
+
   self.user = function({
-    name, email, username, password,
+    name, email,
+    username, password, mfa = false,
     organization, organizations = [],
     group, groups = [],
     metadata = {}
@@ -15,6 +19,7 @@ function Models() {
 
       username,
       password,
+      mfa,
 
       organizations,
       groups,
@@ -32,6 +37,15 @@ function Models() {
 
     return model;
   };
+
+  self.group = function() {};
+  self.organization = function() {}; // mfa required
+  self.organizationUnit = function() {};
+
+  self.ldapUser = function() {};
+  self.ldapGroup = function() {};
+  self.ldapOrganization = function() {};
+  self.ldapOrganizationUnit = function() {};
 
   return self;
 }
