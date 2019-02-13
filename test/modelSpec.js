b633d5f22478dd89c15961f604af519eb8297f19
Mark Barr <mark@hyperingenuity.com>
2019-02-13 17:29:47 -0500

    test: initial test suite and infrastructure added.

---
+++ test/modelSpec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+require('should');
+require('barrkeep');
+const Dapper = require('../lib/dapper');
+
+
+describe('Model Spec', function() {
+  let dapper;
+
+  let user;
+  let ldapUser;
+
+  it('should create a new Dapper instance', function() {
+    dapper = new Dapper();
+    dapper.should.be.ok();
+  });
+
+  it('should create a user object', function() {
+    user = dapper.models.user({
+      username: 'test',
+      password: 'password',
+      name: 'Testy McTest',
+      email: 'test@example.com',
+      organization: 'QA'
+    });
+
+    user.should.be.ok();
+    user.should.have.property('object', 'user');
+  });
+
+  it('should create an ldap user object', function() {
+    ldapUser = dapper.models.ldapUser(user);
+
+    ldapUser.should.be.ok();
+    ldapUser.should.have.property('object', 'ldapUser');
+
+    console.pp(ldapUser);
+  });
+});
