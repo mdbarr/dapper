df665b92b05a7356881126fc8f7fce16989c9fa9
Mark Barr <mark@hyperingenuity.com>
2019-02-18 18:11:34 -0500

    test: additional test cases added with model fixes.

--- test/modelSpec.js
+++ test/modelSpec.js
@@ -14,12 +14,70 @@ describe('Model Spec', function() {
   });
 
   describe('Domain Model Tests', function() {
+    let domain;
+    let ldapDomain;
+
+    it('should create and validate a domain model object', function() {
+      domain = dapper.models.domain({
+        domain: 'test.example.com'
+      });
+
+      domain.should.be.ok();
+      domain.should.have.property('object', 'domain');
+      domain.should.have.property('domain', 'test.example.com');
+    });
+
+    it('should create and validate an ldap domain object', function() {
+      ldapDomain = dapper.models.ldapDomain(domain);
+      ldapDomain.should.be.ok();
+      ldapDomain.should.have.property('object', 'ldapDomain');
+      ldapDomain.should.have.property('dn', 'dc=test, dc=example, dc=com');
+    });
   });
 
   describe('Organization Model Tests', function() {
+    let organization;
+    let ldapOrganization;
+
+    it('should create and validate an organization model object', function() {
+      organization = dapper.models.organization({
+        name: 'Testers'
+      });
+
+      organization.should.be.ok();
+      organization.should.have.property('object', 'organization');
+      organization.should.have.property('name', 'testers');
+    });
+
+    it('should create and validate an ldap organization object', function() {
+      ldapOrganization = dapper.models.ldapOrganization(organization);
+      ldapOrganization.should.be.ok()
+      ldapOrganization.should.have.property('object', 'ldapOrganization');
+      ldapOrganization.should.have.property('dn', 'o=testers');
+    });
   });
 
   describe('Group Model Tests', function() {
+    let group;
+    let ldapGroup;
+
+    it('should create and validate a group object', function() {
+      group = dapper.models.group({
+        name: 'UI Testers',
+        organization: 'Testers'
+      });
+
+      group.should.be.ok();
+      group.should.have.property('object', 'group');
+      group.should.have.property('name', 'ui testers');
+      group.should.have.property('organization', 'testers');
+    });
+
+    it('should create and validate an ldap group', function() {
+      ldapGroup = dapper.models.ldapGroup(group);
+      ldapGroup.should.be.ok();
+      ldapGroup.should.have.property('object', 'ldapGroup');
+    });
   });
 
   describe('User Model Tests', function() {
