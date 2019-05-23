b475c82e4863d11b0f2b2c30e2d1be53c38fc3d2
Mark Barr <mark@hyperingenuity.com>
2019-05-23 16:09:01 -0400

    dapper: basic command line parsing and config file loading

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 
 'use strict';
-
+const path = require('path');
+const minimist = require('minimist');
 const Dapper = require('./lib/dapper');
 
-const dapper = new Dapper();
+const options = minimist(process.argv.slice(2));
+
+let config = {};
+if (options.config) {
+  const configFile = path.resolve(__dirname, options.config);
+  config = require(configFile);
+}
+
+const dapper = new Dapper(config);
 dapper.boot();
