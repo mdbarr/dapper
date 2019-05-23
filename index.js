a327dd4c38dfa1c9db49d359d41dd04cf2c8e6e3
Mark Barr <mark@hyperingenuity.com>
2019-05-23 16:14:04 -0400

    dapper: default config file location and loading

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 #!/usr/bin/env node
 
 'use strict';
+const fs = require('fs');
 const path = require('path');
 const minimist = require('minimist');
 const Dapper = require('./lib/dapper');
 
+const DEFAULT_CONFIG = '/etc/dapper.config.js';
+
 const options = minimist(process.argv.slice(2));
 
+//////////
+// Configuration
+
 let config = {};
-if (options.config) {
-  const configFile = path.resolve(__dirname, options.config);
-  config = require(configFile);
+
+try {
+  if (options.config) {
+    const configFile = path.resolve(__dirname, options.config);
+    config = require(configFile);
+  } else if (fs.existsSync(DEFAULT_CONFIG)) {
+    config = require(DEFAULT_CONFIG);
+  }
+} catch (error) {
+  console.log('Error loading config file:', error.message);
 }
+
+//////////
 
 const dapper = new Dapper(config);
 dapper.boot();
