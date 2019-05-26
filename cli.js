74eac1f42fde2611ef1f9c49397c06940f91a2c1
Mark Barr <mark@hyperingenuity.com>
2019-05-26 10:58:37 -0400

    import: bootstrap renamed to import, cli layout refactor

--- cli.js
+++ cli.js
@@ -5,36 +5,36 @@ const fs = require('fs');
 const path = require('path');
 const minimist = require('minimist');
 const Dapper = require('./lib/dapper');
-const bootstrap = require('./utils/bootstrap');
+const importer = require('./utils/import');
 
 const DEFAULT_CONFIG = '/etc/dapper.config.js';
 
 const options = minimist(process.argv.slice(2));
 
-if (options.bootstrap) {
-  const dataFile = path.resolve(__dirname, options.bootstrap);
+//////////
+// Configuration
+
+let config = {};
+
+try {
+  if (options.config) {
+    const configFile = path.resolve(__dirname, options.config);
+    config = require(configFile);
+  } else if (fs.existsSync(DEFAULT_CONFIG)) {
+    config = require(DEFAULT_CONFIG);
+  }
+} catch (error) {
+  console.log('Error loading config file:', error.message);
+}
+
+//////////
+
+if (options.import) {
+  const dataFile = path.resolve(__dirname, options.import);
   const data = require(dataFile);
 
-  bootstrap(data, options);
+  importer(data, options, config);
 } else {
-  //////////
-  // Configuration
-
-  let config = {};
-
-  try {
-    if (options.config) {
-      const configFile = path.resolve(__dirname, options.config);
-      config = require(configFile);
-    } else if (fs.existsSync(DEFAULT_CONFIG)) {
-      config = require(DEFAULT_CONFIG);
-    }
-  } catch (error) {
-    console.log('Error loading config file:', error.message);
-  }
-
-  //////////
-
   const dapper = new Dapper(config);
   dapper.boot();
 }
